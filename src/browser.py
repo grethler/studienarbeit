c28127a3161753f1bfc19b265b3a52cd2df23d3c
grethler <github.com.darkness063@passmail.net>
2025-01-07 12:50:13 +0100

    iteration

--- src/browser.py
+++ src/browser.py
@@ -8,7 +8,9 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 
@@ -49,26 +51,53 @@ class Browser:
         self.browser.find_element(By.XPATH, path).click()
         sleep(3)
 
+
     def articleIteration(self):
         counter = 0
         num = self.browser.find_element(By.CLASS_NAME, "fup-archive-result-hits").text
         number_of_articles = int(num.split(" ")[0].replace(",", ""))
         self.browser.find_element(By.CLASS_NAME, "fup-archive-result-sort").click()
         sleep(1)
-        while True:
-            elements = self.browser.find_elements(By.CLASS_NAME, "fup-archive-result-item-article")
-            for i in elements:
-                actions = ActionChains(self.browser)
-                actions.move_to_element(i).perform()
-                actions.click(i).perform()
+
+        while counter < 3:  # Change to number_of_articles for full run
+            try:
+                elements = WebDriverWait(self.browser, 10).until(
+                    EC.presence_of_all_elements_located((By.CLASS_NAME, "fup-archive-result-item-article"))
+                )
+
+                for i in elements:
+                    actions = ActionChains(self.browser)
+                    actions.move_to_element(i).perform()
+                    actions.click(i).perform()
+                    sleep(2)
+
+                    WebDriverWait(self.browser, 10).until(
+                        EC.element_to_be_clickable((By.CLASS_NAME, "fup-s-submenu-open"))
+                    ).click()
+
+                    WebDriverWait(self.browser, 10).until(
+                        EC.element_to_be_clickable((By.CLASS_NAME, "fup-s-menu-download-page-confirmation"))
+                    ).click()
+
+                    sleep(2)
+                    self.browser.back()
+                    sleep(2)
+                    counter += 1
+
+                    if counter == 3:  # Change to number_of_articles for full run
+                        break
+
+                if counter < 3:
+                    next_button = WebDriverWait(self.browser, 10).until(
+                        EC.element_to_be_clickable((By.CLASS_NAME, "fup-archive-result-pagination-next"))
+                    )
+                    next_button.click()
+                    sleep(2)
+
+            except StaleElementReferenceException:
+                self.browser.refresh()
                 sleep(2)
-                self.browser.find_element(By.CLASS_NAME, "fup-s-submenu-open").click()
-                self.browser.find_element(By.CLASS_NAME, "fup-s-menu-download-page-confirmation").click()
-                sleep(2)
-                self.browser.back()
-                counter += 1
-            if counter == 3: #== number_of_articles:
-                break
+
         sleep(1000)
 
     def switchTabContext(self):
